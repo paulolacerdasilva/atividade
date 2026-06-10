3198d0d684a61dbbc4819523385127b68dd27941
paulolacerdasilva <plslacerda@gmail.com>
2026-06-10 07:55:54 -0400

    primeiro commit

---
+++ q2.php
@@ -0,0 +1,19 @@
+<?php
+$a1 = 1;
+$a2 = 1;
+$a3 = 2;
+$n = 10;
+if($n < 3 ){
+    echo "A sequência deve ter pelo menos 3 temos";
+}else{
+    echo $a1." ";
+    echo $a2." ";
+    echo $a3." ";
+    for($i = 4; $i <= $n; $i++){
+        $novo = $a1 + $a2 + $a3;
+        echo $novo." ";
+        $a1 = $a2;
+        $a2 = $a3;
+        $a3 = $novo;
+    }
+}
